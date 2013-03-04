2fa40d5d9dcf2c77364a98070068f3f1639cbf0b
Clayton <clayton@claytonlz.com>
2013-03-04 13:38:25 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeofday::Application.config.secret_token = '32435774b4862a5341fb5e588fa13fa881c32a213e4eae215036492169b3c4ea00b3a3aa509a5bf1e24cd32c7f11b148a428e7e6d7a1d747b0ecb949153ac647'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Timeofday::Application.config.session_store :cookie_store, key: '_timeofday_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Timeofday::Application.config.session_store :active_record_store
